316c2eab43b014c21651154c164e097c9ed2b290
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2014-04-07 16:21:44 +0200

    No need for password.

--- upload-backup.rb
+++ upload-backup.rb
@@ -1,7 +1,6 @@
 if ARGV[0] && ARGV[1] && ARGV[2]
 	application_name = ARGV[0]
-	password = ARGV[1]
-	s3_bucket_name = ARGV[2]
+	s3_bucket_name = ARGV[1]
 
 	dump_filename = Time.now.strftime("#{application_name.downcase}-%Y%m-%H")
 
@@ -11,5 +10,5 @@ if ARGV[0] && ARGV[1] && ARGV[2]
 	`tar -zcvf #{backup_file_path}/#{dump_filename}.tar.gz /var/Apps/#{application_name}/production/shared/uploads/`
 	`s3cmd --encrypt --verbose put #{backup_file_path}/#{dump_filename}.tar.gz s3://#{s3_bucket_name}`
 else
-	puts "Usage: #{__FILE__} <application> <password> <s3bucket>"
+	puts "Usage: #{__FILE__} <application> <s3bucket>"
 end
